923ca53558ab546098c8ff07734ea405b0630492
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-19 20:41:28 -0400

    main casi ok

--- app/src/main/java/com/example/dieciseikotlin/MainActivity.kt
+++ app/src/main/java/com/example/dieciseikotlin/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.dieciseikotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dieciseikotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        val adapter = Adapter()
+        adapter.setData(PaisesLatam.paises)
+
+
     }
 }
